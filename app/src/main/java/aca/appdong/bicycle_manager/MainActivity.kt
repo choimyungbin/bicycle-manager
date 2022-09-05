4170e21222f88fb705d26c0b83a7d5122bd692d3
dohun_ <silver2679@gmail.com>
2022-09-06 03:13:34 +0900

    update searchview
    검색 기능 searchview로 구현 추후에 위에 있는 검색 창과 연결할 예정

--- app/src/main/java/aca/appdong/bicycle_manager/MainActivity.kt
+++ app/src/main/java/aca/appdong/bicycle_manager/MainActivity.kt
@@ -1,12 +1,15 @@
 package aca.appdong.bicycle_manager
 
 import aca.appdong.bicycle_manager.databinding.ActivityMainBinding
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SearchView
 import android.util.Log
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item.*
@@ -14,9 +17,10 @@ import kotlinx.android.synthetic.main.item.*
 class MainActivity : AppCompatActivity() {
     lateinit var btnScan: Button
     lateinit var searchInput: EditText
-    lateinit var userItem: UserItem
+    lateinit var userItem: ArrayList<UserItem>
     lateinit var recyclerViewAdapter: RecyclerViewAdapter
-    val datas = mutableListOf<UserItem>()
+    lateinit var search_view: SearchView
+    lateinit var mainRecyclerview: RecyclerView
 //    lateinit var string: String
     var activityMainBinding: ActivityMainBinding? = null
 
@@ -24,7 +28,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding!!.root)
-        initRecycler()
+
+        mainRecyclerview = findViewById(R.id.mainRecyclerview)
+        search_view = findViewById(R.id.search_view)
+        search_view.setOnQueryTextListener(searchViewTextListener)
+
+        userItem = tempUserItem()
+        setAdapter()
         //userItem.(UserItem(1,101,101,123456, true))
         //더미
         btnScan = activityMainBinding!!.buttonScan
@@ -37,18 +47,48 @@ class MainActivity : AppCompatActivity() {
             integrator.initiateScan()   //스캔
         }
     }
-    private fun initRecycler() {
-        recyclerViewAdapter = RecyclerViewAdapter(this)
-        mainRecyclerview.adapter = recyclerViewAdapter
 
-        datas.apply {
-            add(UserItem(1,101,101,123456, true))
+    var searchViewTextListener: SearchView.OnQueryTextListener =
+        object : SearchView.OnQueryTextListener {
+            //검색버튼 입력시 호출, 검색버튼이 없으므로 사용하지 않음
+            override fun onQueryTextSubmit(s: String): Boolean {
+                return false
+            }
 
-            recyclerViewAdapter.datas = datas
-            recyclerViewAdapter.notifyDataSetChanged()
+            //텍스트 입력/수정시에 호출
+            override fun onQueryTextChange(s: String): Boolean {
+                recyclerViewAdapter.filter.filter(s)
+                Log.d(TAG, "SearchVies Text is changed : $s")
+                return false
+            }
         }
-
+    fun setAdapter(){
+        //리사이클러뷰에 리사이클러뷰 어댑터 부착
+        mainRecyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerViewAdapter = RecyclerViewAdapter(userItem, this)
+        mainRecyclerview.adapter = recyclerViewAdapter
     }
+    fun tempUserItem(): ArrayList<UserItem> {
+        var tempUserItem = ArrayList<UserItem>()
+        tempUserItem.add(UserItem(1,101,101,121212,true))
+        tempUserItem.add(UserItem(2,201,102,131313,true))
+        tempUserItem.add(UserItem(3,301,103,242424,true))
+        tempUserItem.add(UserItem(4,401,104,353535,true))
+        tempUserItem.add(UserItem(5,501,105,767676,true))
+        return tempUserItem
+    }
+//    private fun initRecycler() {
+//        recyclerViewAdapter = RecyclerViewAdapter(this)
+//        mainRecyclerview.adapter = recyclerViewAdapter
+//
+//        datas.apply {
+//            add(UserItem(1,101,101,123456, true))
+//
+//            recyclerViewAdapter.datas = datas
+//            recyclerViewAdapter.notifyDataSetChanged()
+//        }
+//
+//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
 
--- app/src/main/java/aca/appdong/bicycle_manager/RecyclerViewAdapter.kt
+++ app/src/main/java/aca/appdong/bicycle_manager/RecyclerViewAdapter.kt
@@ -1,45 +1,130 @@
 package aca.appdong.bicycle_manager
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.ProgressDialog.show
 import android.content.Context
+import android.content.DialogInterface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerViewAdapter(private val context: Context) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter(var userItem: ArrayList<UserItem>, private val context: Context) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(), Filterable {
 
-    var datas = mutableListOf<UserItem>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item,parent,false)
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount(): Int = datas.size
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(datas[position])
-    }
+    var TAG = "RecyclerViewAdapter"
+    var filteredUserItem = ArrayList<UserItem>()
+    var itemFilter = ItemFilter()
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        val itemNumber = view.findViewById<TextView>(R.id.tv_number)
-        val itemBlock = view.findViewById<TextView>(R.id.tv_block)
-        val itemUnit = view.findViewById<TextView>(R.id.tv_unit)
-        val itemBarcode = view.findViewById<TextView>(R.id.tv_barcode)
-        val itemReside = view.findViewById<TextView>(R.id.tv_reside)
+        //        val itemNumber = view.findViewById<TextView>(R.id.tv_number)
+//        val itemBlock = view.findViewById<TextView>(R.id.tv_block)
+//        val itemUnit = view.findViewById<TextView>(R.id.tv_unit)
+//        val itemBarcode = view.findViewById<TextView>(R.id.tv_barcode)
+//        val itemReside = view.findViewById<TextView>(R.id.tv_reside)
+//        fun bind(item: UserItem) {
+//            itemNumber.text = item.number.toString()
+//            itemBlock.text = item.block.toString()
+//            itemUnit.text = item.unit.toString()
+//            itemBarcode.text = item.barcode.toString()
+//            itemReside.text = item.reside.toString()
+//        }
+        lateinit var tv_number: TextView
+        lateinit var tv_block: TextView
+        lateinit var tv_unit: TextView
+        lateinit var tv_barcode: TextView
+        lateinit var tv_reside: TextView
 
+        init {
+            tv_number = itemView.findViewById(R.id.tv_number)
+            tv_block = itemView.findViewById(R.id.tv_block)
+            tv_unit = itemView.findViewById(R.id.tv_unit)
+            tv_barcode = itemView.findViewById(R.id.tv_barcode)
+            tv_reside = itemView.findViewById(R.id.tv_reside)
 
-        fun bind(item: UserItem) {
-            itemNumber.text = item.number.toString()
-            itemBlock.text = item.block.toString()
-            itemUnit.text = item.unit.toString()
-            itemBarcode.text = item.barcode.toString()
-            itemReside.text = item.reside.toString()
+//            itemView.setOnClickListener {
+//                AlertDialog.Builder(context).apply {
+//                    var position = adapterPosition
+//                    var user = filteredUserItem[position]
+//                    setTitle(user.number)
+//                    setMessage(user.phoneNumber)
+//                    setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+//                        Toast.makeText(context, "OK Button Click", Toast.LENGTH_SHORT).show()
+//                    })
+//                    show()
+//                }
+//            }
         }
     }
 
+    init {
+        filteredUserItem.addAll(userItem)
+    }
 
+    override fun getItemCount(): Int {
+        return filteredUserItem.size
+    }
+
+    override fun getFilter(): Filter {
+        return itemFilter
+    }
+
+    inner class ItemFilter : Filter() {
+        override fun performFiltering(charSequence: CharSequence): FilterResults {
+            val filterString = charSequence.toString()
+            val results = FilterResults()
+            Log.d(TAG, "charSequence : $charSequence")
+
+            //검색이 필요없을 경우를 위해 원본 배열을 복제
+            val filteredList: ArrayList<UserItem> = ArrayList<UserItem>()
+            //공백제외 아무런 값이 없을 경우 -> 원본 배열
+            if (filterString.trim { it <= ' ' }.isEmpty()) {
+                results.values = userItem
+                results.count = userItem.size
+
+                return results
+                //공백제외 2글자 이인 경우 -> 이름으로만 검색
+            } else {
+                for (useritem in userItem) {
+                    if (useritem.barcode.toString().contains(filterString)) {
+                        filteredList.add(useritem)
+                    }
+                }
+                //그 외의 경우(공백제외 2글자 초과) -> 이름/전화번호로 검색
+            }
+            results.values = filteredList
+            results.count = filteredList.size
+
+            return results
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults) {
+            filteredUserItem.clear()
+            filteredUserItem.addAll(filterResults.values as ArrayList<UserItem>)
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userItem: UserItem = filteredUserItem[position]
+
+        holder.tv_number.text = userItem.number.toString()
+        holder.tv_block.text = userItem.block.toString()
+        holder.tv_unit.text = userItem.unit.toString()
+        holder.tv_barcode.text = userItem.barcode.toString()
+        holder.tv_reside.text = userItem.reside.toString()
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val con = parent.context
+        val inflater = con.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.item, parent, false)
+
+        return ViewHolder(view)
+    }
 }
